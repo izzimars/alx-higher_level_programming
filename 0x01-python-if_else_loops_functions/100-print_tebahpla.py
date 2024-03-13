ff7831fa7bc3e1f186851c01e4fec0baeb7404e2
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-13 06:25:09 -0700

    0x01 tk 14 fix 00

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+count = 122
+while (count>=97):
+    if (count % 2 == 0):
+        num = count
+    else:
+        num = count - 32
+    count = count - 1
+    print("{}".format(chr(num)), end="")
