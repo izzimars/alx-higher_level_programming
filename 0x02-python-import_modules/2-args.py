538ffd2cbebcb3d02a4cbcbe803812d3e0418763
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-14 17:33:15 -0700

    0x02 tk 02 fix 04

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
- 
     arguments = sys.argv
     ls = arguments[1:]
     i = 1
+
     if len(ls) == 0:
-        print("{} argument.".format(len(ls)))
+        print("{} arguments.".format(len(ls)))
     else:
         if (len(ls) == 1):
             print("{} argument:".format(len(ls)))
