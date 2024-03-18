a00d51c2541a5c5d87485072e0166f2a8baaf644
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-18 02:27:48 -0700

    0x03 tk 04 fix 00

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """A function that copies a listand replacaes an element  at a particular index.
+
+    Args:
+        my_list: The list to be indexed
+        idx: the index
+
+    Returns:
+        A list.
+    """
+    if (idx < 0 or idx >= len(my_list)):
+        return my_list[:]
+    else:
+        list_new = my_list[:]
+        list_new[idx] = element
+        return list_new
