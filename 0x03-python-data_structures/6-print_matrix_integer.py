5758f155eae92e1099e5ce35d1119c04afbdb201
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-18 08:01:55 -0700

    0x03 tk 06 fix 01

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
-    else:
-        for i in matrix:
-            for j in i:
-                if j == i[-1]:
-                    print("{}".format(j))
-                else:
-                    print("{}".format(j), end=' ')
+    pass
