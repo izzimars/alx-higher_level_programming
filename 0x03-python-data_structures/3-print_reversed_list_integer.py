9e914b24a36456c293c0eab90108fcc47a4c3cbc
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-18 02:17:36 -0700

    0x03 tk 03 fix 01

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,7 +8,10 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         None
     """
-    indx = len(my_list) - 1
-    while (indx >= 0):
-        print("{:d}".format(my_list[indx]))
-        indx = indx - 1
+    if not my_list:
+        pass
+    else:
+        indx = len(my_list) - 1
+        while (indx >= 0):
+            print("{:d}".format(my_list[indx]))
+            indx = indx - 1
