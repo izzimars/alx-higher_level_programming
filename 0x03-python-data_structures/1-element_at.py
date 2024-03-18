59c700a81e148c638d6d0e70ae819f1a446186c2
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-17 17:41:59 -0700

    0x03 tk 01 -fix 00

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """A function that mimmic the way 
+    indexing a function in c is
+
+    Args:
+        my_list: The list to be indexed
+        idx: the index
+
+    Returns:
+        None
+    """
+    if (idx >= len(mylist) or idx < 0):
+        return (None)
+    else:
+        return (my_list[idx])
