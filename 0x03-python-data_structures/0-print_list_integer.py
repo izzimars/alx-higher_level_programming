9c5f839d786f31d8169fddb94b792a3182ede066
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-17 17:29:18 -0700

    0x03 tk 00 -fix 02

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
+    """Mylist printing function
+
+    Args:
+        my_list: The list to be printed
+
+    Returns:
+        None
+    """
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
