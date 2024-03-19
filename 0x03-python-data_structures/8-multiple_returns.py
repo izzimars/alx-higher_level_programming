8160b989e165e96c1f315d0ff6f53855c56df7dd
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-19 10:24:14 -0700

    0x03 tk 08 fix 01

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,5 @@
 #!bin/bash/python3
 def multiple_returns(sentence):
-    """A function that tries the multiple return power of a tuple.
-
-    Args:
-        sentence: string
-
-    Return: tuple
-    """
     lent = len(sentence)
     if lent == 0:
         return (lent, None)
