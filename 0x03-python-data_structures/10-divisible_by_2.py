71c842ab4cb4ff41c6c2fd31d15afea63e0e295b
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-19 13:33:14 -0700

    0x03 tk 10 fix 00

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -6,5 +6,6 @@ def divisible_by_2(my_list=[]):
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
             new_list.append(True)
+            continue
         new_list.append(False)
     return new_list
