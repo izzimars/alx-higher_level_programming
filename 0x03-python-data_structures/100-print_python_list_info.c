524da67ab5d8aa30b1bbeeb8641e73325cfb801e
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-22 08:31:21 -0700

    0x03 tk 100 fix -01

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,18 +9,17 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t listsze;
+	PyListObject listsze;
 	long size, i;
 	PyObject *item;
 
-	size = Py_SIZE(listsze);
+	size = Py_SIZE(p);
 	listsze = PyList_Size(p);
-	printf("[*] Size of the Python List = %d\n", lstsze);
-	printf("[*] Allocated = %d\n", size);
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", listsze);
 	for (i = 0; i < listsze; i++)
 	{
 		item = PyList_GetItem(p, i);
 		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
-
