ceee623c17dc092df091a2a380e32fe212dfa697
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-23 02:55:51 -0700

    fixing 01

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     new_list = set(my_list)
     new_list = list(new_list)
-    summax = 0;
+    summax = 0
     for i in new_list:
         summax = summax + i
     return summax
