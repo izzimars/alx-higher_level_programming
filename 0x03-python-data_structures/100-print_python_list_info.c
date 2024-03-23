c5638a55d3cd2b755399e822b0de537b36b06a78
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-23 02:23:37 -0700

    fixing III

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,7 +14,7 @@ void print_python_list_info(PyObject *p)
 	size = Py_SIZE(p);
 	alloc = ((PyListObject *)p)->allocated;
 	printf("[*] Size of the Python List = %d\n", size);
-	printf("[*] Allocated = %d", alloc);
+	printf("[*] Allocated = %d\n", alloc);
 	for (i = 0; i < size; i++)
 	{
 		printf("Element %d: ", i);
