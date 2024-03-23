4d9a20a5eab6f8265ecd86fe29aab7a3cb22b785
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-23 02:54:28 -0700

    fixing 00

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    ind = new_list.index(search)
-    new_list[ind] = replace
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            ind = new_list.index(search)
+            new_list[ind] = replace
     return new_list
