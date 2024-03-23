5207ce53a19eda0988488014a8672e4e349141ee
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-23 03:08:32 -0700

    fixing 03

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None | len(a_dictionary) == 0:
+    if a_dictionary is None:
+        return None
+    if len(a_dictionary) == 0:
         return None
     new_list = list(a_dictionary.keys())
     maxi = 0
