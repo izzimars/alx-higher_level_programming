965f687241814447f324106fd7fb1aa1ae3700e6
Macaulay Israel <israelmacaulay8@gmail.com>
2024-03-23 02:59:29 -0700

    fixing 02

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
+    """ A funtion that performs intersection on two list
+
+    Args:
+        1)set_1: a list type argument
+        2)set_2: a list type argument
+
+    Return:
+        -]A new list item with the intersection result
+    """
     new_list = list(set_1.intersection(set_2))
     return new_list
