443cb0ddc26f4b6344ae923335c3214849c5a375
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-11 03:06:03 -0700

    0x08 tk 01 fix 02

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,5 +30,5 @@ class Rectangle:
             if not isinstance(width, int):
                 raise TypeError("width must be >= 0")
             if width < 0:
-                raise ValueError("height must be >= 0")
+                raise ValueError("width must be >= 0")
             self.__width = width
