39c1f6d74a9a9fa0b566c2c0e760797a896314dd
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-11 03:33:40 -0700

    0x08 tk 02 fix 00

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" module rectangle"""
+
+
+class Rectangle:
+    """ class rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @height.setter
+    def height(self, height):
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+    @width.setter
+    def width(self, width):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        return (self.__height + self.__width) * 2
