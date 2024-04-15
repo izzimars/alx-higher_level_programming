1fae39e2f21dc6d06dc5a9fd557bb798f489a22a
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-15 06:37:23 -0700

    0x0A tk 00 fix 01

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""
+Returns the list of available attributes and methods of an object.
+
+
+:param obj: The object to checks its property.
+:type obj: object.
+:rtype: list.
+"""
+
+
 def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object.
-
-
-    :param obj: The object to checks its property.
-    :type obj: object.
-    :rtype: list.
-    """
+    """returns the list of available attributes"""
     return dir(obj)
