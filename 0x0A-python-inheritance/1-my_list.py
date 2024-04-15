ef6b51a7e662713f8521e65322b99e00b4c054fb
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-15 07:56:46 -0700

    0x0A tk 01 fix 00

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+MyList Module
+=============
+
+This module defines the MyList class, which inherits from
+the built-in list class.
+
+Classes:
+    MyList: A subclass of list with additional functionality.
+
+Public Function:
+    None.
+
+"""
+
+
+class MyList(list):
+    """
+    MyList Class
+
+    A subclass of list with additional functionality.
+
+    Public Methods:
+        print_sorted(): Print the elements of the list in ascending order.
+
+    """
+
+    def print_sorted(self):
+        """
+        Print the elements of the list in ascending order.
+
+        This method sorts the elements of the list in ascending order
+        and prints the sorted list.
+
+        Returns:
+            None
+
+        """
+
+        b = self[:];
+        b.sort()
+        print(b)
