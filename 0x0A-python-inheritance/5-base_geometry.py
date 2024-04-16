4b6f03605106630cbb4bf3bbb39ebff93e8d7cf9
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-15 17:47:55 -0700

    0x0A tk 05 fix 00

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+base_geometry Module
+====================
+
+This module defines the MyList class, which inherits from
+the built-in list class.
+
+BaseGeometry:
+    MyList: A subclass of list with additional functionality.
+
+Public Function:
+    None.
+
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry Class
+
+    A subclass of list with additional functionality.
+
+    Public Methods:
+        None.
+
+    """
+
+    pass
