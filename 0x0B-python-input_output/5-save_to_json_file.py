5128939bb234310c47db448b9beeb47949a8ed4a
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-16 19:21:10 -0700

    0x0B tk 05 fix 02

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -29,5 +29,5 @@ def save_to_json_file(my_obj, filename):
         None.
     """
 
-    with open("filename", "w") as txt:
-        json.dump(my_obj, txt)
+    with open(filename, "w") as txt:
+        txt.write(json.dumps(my_obj))
