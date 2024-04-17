59183762b2fcc9986ea9bb956d1213f5c1e347aa
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-16 18:25:30 -0700

    0x0B tk 00 fix 00

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 """
-read_file Module
-================
-
-This module defines the is_kind_of_class class, which inherits from
-the object class.
-    is_kind_of_class: A new class without a formal base class.
+write_file Module
+=================
 
 Public Function:
     is_kind_of_class: Apublic function that checks if an object
@@ -14,22 +10,21 @@ Public Function:
 """
 
 
-def read_file(filename=""):
+def write_file(filename="", text=""):
     """
-    A function that reads a text file.
+    A function that writes a text into a file.
 
     This method reads a file and print it out to the standard
     output.
 
     :params filename: a computer file.
     :type file: mostly a .txt file or any kind of file.
+    :params tsxt: a string of words to be written to filename.
+    :type str: a string.
 
     Returns:
         None
-
-
     """
 
-    with open(filename, 'r', encoding="utf-8") as txt:
-        for line in txt:
-            print(line, end="")
+    with open(filename, 'w', encoding="utf-8") as txt:
+        return txt.write(text)
