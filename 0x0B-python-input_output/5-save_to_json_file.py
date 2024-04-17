a2494e951c0e6c50de1b3bd413d221bfbe020ee2
Macaulay Israel <israelmacaulay8@gmail.com>
2024-04-16 19:11:58 -0700

    0x0B tk 05 fix 00

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+save_to_json_file Module
+========================
+
+Public Function:
+    is_kind_of_class: Apublic function that checks if an object
+    belongs to the same kind of class or subclass.
+
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    A function that writes a json object into a file.
+
+    This method returns the erite the json equivalent of a python
+    inside a file.
+
+    :params my_obj: a python object.
+    :type object: python object, positional arguments.
+    :params filename: a file to write the json equivalent object.
+    :type file: a computer file mostly a .txt file, a positional
+    argument.
+
+    Returns:
+        None.
+    """
+
+    with open("filename", "w") as txt:
+        txt.write(json.dumps(my_obj))
