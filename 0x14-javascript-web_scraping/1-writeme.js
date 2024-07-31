421aacf77163c6aafd9a839b089c6a5dfef19358
izzimars <israelmacaulay8@gmail.com>
2024-07-30 19:11:01 -0700

    lets see

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const file = process.argv[2];
 const content = process.argv[3]
 
-fs.writeFile(file, content, 'utf-8', function (err, data) {
+fs.writeFile(file, content, 'utf-8', function (err) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
   }
 });
